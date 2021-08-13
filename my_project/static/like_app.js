f9b584fa6be10e890df00e0f3b405c8cf69b34fa
Jisoo Choi <jisoochoi7561@gmail.com>
2021-08-13 14:22:41 +0900

    temp

--- my_project/static/like_app.js
+++ my_project/static/like_app.js
@@ -1,4 +1,4 @@
-import "./like_button";
+import LikeButton from "./like_button";"./like_button";
 
 var domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(React.createElement(LikeButton, null), domContainer);
