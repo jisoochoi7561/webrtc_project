e05271e89f1dd39080c5ef76de9a06d0a9a22fef
Jisoo Choi <jisoochoi7561@gmail.com>
2021-08-15 16:36:18 +0900

    select to whisper

--- my_project/static/js/admin.js
+++ my_project/static/js/admin.js
@@ -172,8 +172,7 @@ function startCall(studentName,roomName){
 			my_label.addEventListener('click', function() {
 				document.getElementById("toSelect").selectedIndex = 2;
 				document.getElementById("toSpecific").style.display = "inline-block";
-				console.log(my_label.innerHTML)
-				document.getElementById("toSpecific").innerHTML = my_label.innerHTML
+				document.getElementById("toSpecific").value = my_label.innerHTML
 			});
 			document.getElementById('videoLists').appendChild(my_student_element)
 		}
@@ -271,7 +270,7 @@ function camStartCall(camName,roomName){
 			my_label.addEventListener('click', function() {
 				document.getElementById("toSelect").selectedIndex = 2;
 				document.getElementById("toSpecific").style.display = "inline-block";
-				document.getElementById("toSpecific").innerHTML = my_label.innerHTML
+				document.getElementById("toSpecific").value = my_label.innerHTML
 			});
 			document.getElementById('videoLists').appendChild(my_student_element)
 		}
