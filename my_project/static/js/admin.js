b0fddd495e45ae99c178faf90298f8c897594bcf
jisoochoi7561 <jisoochoi7561@gmail.com>
2021-12-01 15:33:56 +0900

    move like_button to admin work pls..

--- my_project/static/js/admin.js
+++ my_project/static/js/admin.js
@@ -516,3 +516,26 @@ function specificSelected(that) {
     document.getElementById("toSpecific").style.display = "none";
   }
 }
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
+    }
+
+    return e(
+      "button",
+      { onClick: () => this.setState({ liked: true }) },
+      "Like"
+    );
+  }
+}
+const domContainer = document.querySelector("#like_button_container");
+ReactDOM.render(e(LikeButton), domContainer);
